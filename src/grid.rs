c829a2d1c353eef0b9f96a130d372e05246b2d1c
DEVAN1220 <darshandevan50@gmail.com>
2024-09-24 17:28:54 +0530

    used clippy

--- src/grid.rs
+++ src/grid.rs
@@ -60,12 +60,8 @@ impl Grid {
                 match self.cells[y][x].cell_type {
                     CellTypes::Air => (),
                     CellTypes::Sand => {
-                        if y < COLUMN as usize  { 
-                            if !self.move_to(x, y, x, y+1) && x >  0 {
-                                if !self.move_to(x, y, x-1, y+1) && x + 1 < ROW as usize {
-                                    self.move_to(x, y, x+1, y+1);
-                                }
-                            }
+                        if y < COLUMN as usize && !self.move_to(x, y, x, y+1) && x >  0 && !self.move_to(x, y, x-1, y+1) && x + 1 < ROW as usize {
+                            self.move_to(x, y, x+1, y+1);
                         }
                     }
                 }
